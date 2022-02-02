138efb96f3498a60f27e78b25ca28ab1b1ec1577
Daniel Kahlenberg <573@users.noreply.github.com>
2022-02-02 22:13:56 +0100

    Update shell.nix
    Try shellFor and callCabal2nix acc. to https://github.com/expipiplus1/nix-repro-86775/blob/master/shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,10 @@
 { project ? import ./nix { }
 }:
+let
+  drv = project.pkgs.haskellPackages.callCabal2nix "" src { };
 
-project.pkgs.mkShell {
+in project.pkgs.haskellPackages.shellFor {
+  packages = _: [ drv ];
   buildInputs = builtins.attrValues project.devTools;
   shellHook = ''
     ${project.ci.pre-commit-check.shellHook}
@@ -9,3 +12,11 @@ project.pkgs.mkShell {
     NIX_ENFORCE_PURITY=0 cabal --enable-nix new-build --constraint 'lukko -ofd-locking' 
   '';
 }
+#project.pkgs.mkShell {
+#  buildInputs = builtins.attrValues project.devTools;
+#  shellHook = ''
+#    ${project.ci.pre-commit-check.shellHook}
+#    ghc -e 'putStrLn ("Architecture: " ++ System.Info.arch)'
+#    NIX_ENFORCE_PURITY=0 cabal --enable-nix new-build --constraint 'lukko -ofd-locking' 
+#  '';
+#}
