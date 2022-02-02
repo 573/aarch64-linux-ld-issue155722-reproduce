a7b3ac2de762ef04373abc04aa391e779c7cf7e3
Daniel Kahlenberg <573@users.noreply.github.com>
2022-02-02 21:24:21 +0100

    Update shell.nix
    Get out more about architecture. Is `system aarch64-linux` applied ?

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ project.pkgs.mkShell {
   buildInputs = builtins.attrValues project.devTools;
   shellHook = ''
     ${project.ci.pre-commit-check.shellHook}
+    ghc -e 'putStrLn ("Architecture: " ++ System.Info.arch)'
     NIX_ENFORCE_PURITY=0 cabal --enable-nix new-build
   '';
 }
